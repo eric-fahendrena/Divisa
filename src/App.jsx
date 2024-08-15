afa62396ff17ef42d6fd600b9255e386af5b5f7d
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-08-15 20:05:54 +0300

    added metadatas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Helmet } from 'react-helmet'
 import Header from './components/Header.jsx'
 import Exchange from './components/Exchange.jsx'
 
@@ -7,6 +8,13 @@ function App() {
 
   return (
     <>
+      <Helmet>
+        <title>Divisa Exchange</title>
+        <meta name='description' content='Obtenez les taux de change en temps réel pour toutes vos conversions de devises.' />
+        <meta name='keywords' content='Divisa Exchange, Conversion de devises en ligne, Taux de change en temps réel, Convertisseur de monnaies, Calculateur de change, Convertisseur EUR en USD, Outil de conversion de devises' />
+        <meta property='og:title' content='Divisa Exchange - Convertisseur de devises' />
+        <meta property='og:description' content='Divisa Exchange vous permet de convertir instantanément vos devises avec les taux les plus récents.' />
+      </Helmet>
       <Header />
       <Exchange />
     </>
